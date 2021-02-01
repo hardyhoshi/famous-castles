44cc908727b331a511e889a76826b5986212d8ad
hardyhoshi <hardyhoshi@gmail.com>
2021-02-01 18:35:27 +0900

    @post = Post.find(params[:id])をまとめた

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,10 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+
   def index
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -18,17 +19,19 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
   
   def update
-    @post = Post.find(params[:id])
     @post.update(post_params)
     redirect_to post_path(@post)
   end
 
   private
-    def post_params
-      params.require(:post).permit(:name, :body, :image)
-    end
+  def post_params
+    params.require(:post).permit(:name, :body, :image)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
