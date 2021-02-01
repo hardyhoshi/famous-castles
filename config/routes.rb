49727b32617587167101260ecb6de00e53557d9d
hardyhoshi <hardyhoshi@gmail.com>
2021-02-01 15:16:44 +0900

    ルーティングにupdateを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
-  resources :users, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit, :update]
 end
