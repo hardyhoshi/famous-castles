5bf3cee6f1d41bfc25705e2d7c994e8ce441bed8
hardyhoshi <hardyhoshi@gmail.com>
2021-02-01 16:29:11 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/new'
-  get 'posts/edit'
   devise_for :users
   root to: "home#index"
   resources :users, only: [:index, :show, :edit, :update]
+  resources :posts
 end
