a91e5e9efb970fb3bab1c93a3b035a2d47208367
hardyhoshi <hardyhoshi@gmail.com>
2021-02-01 18:10:42 +0900

    updateアクションを記述

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,12 @@ class PostsController < ApplicationController
   def edit
     @post = Post.find(params[:id])
   end
+  
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post)
+  end
 
   private
     def post_params
