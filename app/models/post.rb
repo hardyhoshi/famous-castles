ba1f9754a3e7c2a54c23ae06d41adcc296b510b5
hardyhoshi <hardyhoshi@gmail.com>
2021-02-02 12:22:08 +0900

    バリデーションの設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :user
   attachment :image
+
+  with_options presence: true do
+    validates :name
+    validates :body
+    validates :image
+  end
+  
 end
