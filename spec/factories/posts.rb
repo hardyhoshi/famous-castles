adb0c7a004a33815bdf3bfd7600dfcbfac8398a7
hardyhoshi <hardyhoshi@gmail.com>
2021-02-15 19:08:42 +0900

    それぞれのテストについて記述

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :post do
     name     {Faker::Lorem.sentence}
     body     {Faker::Lorem.sentence}
-    image_id {Faker::Lorem.sentence}
+    image_id {123456}
     association :user
   end
 end
