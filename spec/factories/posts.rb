34d1481f5ef963e393a07788c8b1497c8a47a404
hardyhoshi <hardyhoshi@gmail.com>
2021-02-15 19:19:03 +0900

    rubocopの修正

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :post do
-    name     {Faker::Lorem.sentence}
-    body     {Faker::Lorem.sentence}
-    image_id {123456}
+    name     { Faker::Lorem.sentence }
+    body     { Faker::Lorem.sentence }
+    image_id { 123_456 }
     association :user
   end
 end
